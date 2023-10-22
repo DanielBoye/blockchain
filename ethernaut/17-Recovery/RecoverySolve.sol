ec9bff9f8572674eee5cd975f12ac5be31eafed9
Daniel Boye <danielboye888@gmail.com>
2023-10-22 19:34:38 +0200

    Recovery solve

---
+++ ethernaut/17-Recovery/RecoverySolve.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract find {
+    function recover(address sender) external pure returns (address) {
+       address addr = address(uint160(uint256(keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), sender, bytes1(0x01))))));
+       return addr;
+    }
+}
