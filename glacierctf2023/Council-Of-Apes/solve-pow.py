02db6b25bc14318abffd72b9c36a0ab8c76694e3
DanielBoye <danielboye888@gmail.com>
2024-01-22 08:52:09 +0100

    add council-of-apes challenge

---
+++ glacierctf2023/Council-Of-Apes/solve-pow.py
@@ -0,0 +1,14 @@
+import hashlib,random
+x = 100000000+random.randint(0,200000000000)
+for i in range(x,x+20000000000):
+	m = hashlib.sha256()
+	ticket = str(i)
+	# print(ticket)
+	m.update(ticket.encode('ascii'))
+	digest1 = m.digest()
+	m = hashlib.sha256()
+	m.update(digest1 + ticket.encode('ascii'))
+	# print(m.hexdigest())
+	if m.hexdigest().startswith('0000000'):
+		print(i)
+		break
