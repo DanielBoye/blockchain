db49613b36b191755421f576b454479f4f63f40c
DanielBoye <danielboye888@gmail.com>
2024-03-02 23:54:32 +0100

    Update Dex-TwoSolve.sol

---
+++ ethernaut/23-Dex-Two/Dex-TwoSolve.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Solve {
+    
+}
+
+
+interface IDex {
+    function token1() external view returns (address);
+    function token2() external view returns (address);
+    function getSwapPrice(address from, address to, uint256 amount) external view returns(uint);
+    function swap(address from, address to, uint256 amount) external;
+}
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+}
